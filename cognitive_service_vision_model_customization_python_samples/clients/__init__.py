6d02fb21fa777586ad20fc39fa66c560de212a45
KUAN LU <kualu@microsoft.com>
2023-05-06 15:23:42 -0700

    expose client

--- cognitive_service_vision_model_customization_python_samples/clients/__init__.py
+++ cognitive_service_vision_model_customization_python_samples/clients/__init__.py
@@ -2,6 +2,6 @@ from .training_client import TrainingClient
 from .dataset_client import DatasetClient
 from .prediction_client import PredictionClient
 from .evaluation_client import EvaluationClient
-from .client import ResourceType
+from .client import Client, ResourceType
 
-__all__ = ['TrainingClient', 'DatasetClient', 'PredictionClient', 'EvaluationClient', 'ResourceType']
+__all__ = ['TrainingClient', 'DatasetClient', 'PredictionClient', 'EvaluationClient', 'Client', 'ResourceType']
